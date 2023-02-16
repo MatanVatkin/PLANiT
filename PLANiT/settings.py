47aee9bee64486691192e6dc4f360b3f4b796304
Matan Vatkin <matanvatkin@gmail.com>
2023-02-16 19:45:01 +0200

    changed debug

--- PLANiT/settings.py
+++ PLANiT/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hj8m)958-!eakv_w@jqfl)(51qf%ij$0q4%31#i-oqstw329(m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 

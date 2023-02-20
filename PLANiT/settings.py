46f5ebe6d69f4aa2b13e30426aac665c54e9b57a
Matan Vatkin <matanvatkin@gmail.com>
2023-02-20 23:37:33 +0200

    testing #5

--- PLANiT/settings.py
+++ PLANiT/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ['DEBUG']) # 1 == True
+DEBUG = os.environ['DEBUG']
 print(f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!dev mode /////////{SECRET_KEY}")
 
 ALLOWED_HOSTS = []
